79651bf3f7bdad0156a25b083b18eaab814765c4
Meghaa K <140928258+Meghak1@users.noreply.github.com>
2023-10-28 20:15:05 +0530

    Update Ugly number.cpp
    Leetcode solution to problem Ugly Number

--- Ugly number.cpp
+++ Ugly number.cpp
@@ -1,1 +1,20 @@
-
+/*The ugly number is a positive integer
+whose prime factors are limited to 2,3,5.
+  Given a integer neturn true if n is 
+ugly number*/
+class Solution {
+public:
+bool isUgly(int n) {
+  if(n<=0) return false;
+while(n%2==0) {
+n/=2;
+}
+while(n%3==0) {
+n/=3;
+}
+while(n%5==0) {
+n/=5;
+}
+return n==1;
+}
+};
