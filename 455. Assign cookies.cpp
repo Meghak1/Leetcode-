c44b891cf35c286503b67e2b7b1d315409665c43
Meghaa K <140928258+Meghak1@users.noreply.github.com>
2024-01-01 15:25:03 +0530

    Create 455. Assign cookies.cpp

---
+++ 455. Assign cookies.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int contentChildren=0;
+        for(int i=0,j=0;i<g.size() && j<s.size(); ++j) {
+            if(s[j]>=g[i]) 
+            {
+                ++contentChildren;
+                ++i;
+             }
+    }
+ return contentChildren;
+  }
+};
